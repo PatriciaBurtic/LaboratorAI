2a42ea0efeed6b6ab57b6ce24d6b5ad083986970
Patricia Burtic <patricia.burtic@gmail.com>
2021-03-08 19:06:41 +0200

    Tema 2 done

--- Lab2/tsp.py
+++ Lab2/tsp.py
@@ -6,12 +6,14 @@ def minimumDistance(noCities, distances, currentPath):
     currentCity = -sys.maxsize -1
     cities = range(1,n+1)
 
+    #search nearest neighbour which is not already in the path
     for city in cities:
         distance = distances[city-1]
         if distance < currentMinimum and city not in currentPath and distance != 0:
             currentCity = city
             currentMinimum =  distances[city-1]
     
+    #return nearest neighbour and the cost to travel to it
     return currentCity, currentMinimum
             
 
@@ -20,6 +22,7 @@ def searchShortestPath(noCities,source,destination,matrix):
     cost = 0
     path = [source]
     
+    #search a path to destination until we reach it or the length of the path is equal to the initial number of cities
     while len(path) != noCities:
         city, minDist = minimumDistance(noCities, matrix[path[-1]-1], path)
         cost += minDist
@@ -35,18 +38,12 @@ def searchShortestPath(noCities,source,destination,matrix):
 
 
 def visitAllCities(n, matrix):
-    minimumCost = sys.maxsize
-    shortestPath = []
-
-    for city in range(1,n+1):
-        path,cost = searchShortestPath(n,city,city,matrix)
-        if cost < minimumCost:
-            minimumCost = cost
-            shortestPath = path
-    
-    return minimumCost, shortestPath
+    #visit all cities and return the path and its cost, from city number 1 
+    path,cost = searchShortestPath(n,1,1,matrix)
+    return cost, path
 
 def sourceToDestination(n, source, destination, matrix):
+    #find shortest path from a specific source to a specific destination and its cost
     shortestPath, minimumCost = searchShortestPath(n,source,destination,matrix)
     return minimumCost, shortestPath
 
@@ -54,9 +51,9 @@ def sourceToDestination(n, source, destination, matrix):
 
 if __name__ == '__main__':
     matrix=[]
-    f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\easy_01_tsp.txt",'r')
+    #f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\easy_01_tsp.txt",'r')
     #f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\medium_01_tsp.txt",'r')
-    #f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\hard_01_tsp.txt",'r')
+    f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\hard_01_tsp.txt",'r')
     n = int(f.readline())
     for i in range(n):   
         linie = [int(num) for num in f.readline().split(',')]
@@ -64,7 +61,7 @@ if __name__ == '__main__':
     source = int(f.readline())
     destination = int(f.readline())   
     
-    f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\easy_01_my_solution.txt",'w')
+    f = open("D:\\UBB_info_sem_4\\AI\\LAB\\Teme\\Lab2\\my_solution.txt",'w')
 
     minimumCost, shortestPath = visitAllCities(n,matrix)
     
